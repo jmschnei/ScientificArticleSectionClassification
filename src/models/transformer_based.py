92a55039e29db576333f0e5b9e752c28cb7d2cb4
Julian Moreno Schneider <jmschnei@gmail.com>
2023-12-28 12:53:34 +0100

    First empty file for classification methods

---
+++ src/models/transformer_based.py
@@ -0,0 +1,22 @@
+from mydata import *
+
+def classify_sections_bert(sect):
+
+    # We have to get the text of the section
+
+
+    # We have to store the labels
+
+
+    return sect
+
+
+def classify_sections_scibert(sect):
+    # TODO method to be implemented
+    pass
+
+
+def classify_sections_roberta(sect):
+    # TODO method to be implemented
+    pass
+
