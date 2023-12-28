c314b071f7d1947f0fe3c0456aaf54e557a81ecc
Julian Moreno Schneider <jmschnei@gmail.com>
2023-12-28 14:46:25 +0100

    Cleaning file

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,11 +1,8 @@
 #encoding=utf-8
-
-
 import argparse
 import time
 import os
 import papers_section_data_builder
-
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -36,29 +33,20 @@ if __name__ == '__main__':
     parser.add_argument('-lower_percentage', default='5')
 
     #############################################################################################
-    
-    
-    
     parser.add_argument("-base_LM", default='bert-base', type=str, choices=['bert-base', 'bert-large', 'roberta-base','longformer-base-4096','longformer-large-4096','bigbird-pegasus-large-arxiv','bigbird-pegasus-large-pubmed'])
     parser.add_argument("-temp_dir", default='temp')
     parser.add_argument("-corenlp_path", default='', type=str)#
     parser.add_argument("-vocab_file", default='', type=str)#
     parser.add_argument("-obtain_tok_se", type=str2bool, nargs='?',const=True,default=True)#
     
-    
-    
     parser.add_argument("-summ_select_mode", default='greedy', type=str)
     parser.add_argument("-summ_size", default=3, type=int)#
     
     parser.add_argument("-map_path", default='../../data/')
     
-    
     parser.add_argument("-tok_sent_path", default='../../data/')#
     parser.add_argument("-tok_para_path", default='../../data/')#
     parser.add_argument("-histruct_path", default='../../data/')#
-    
-    
-    
 
     parser.add_argument("-shard_size", default=2000, type=int)
     parser.add_argument('-min_src_nsents', default=3, type=int)#0,3
@@ -70,26 +58,13 @@ if __name__ == '__main__':
 
     parser.add_argument("-lower", type=str2bool, nargs='?',const=True,default=True)#
     parser.add_argument("-use_bert_basic_tokenizer", type=str2bool, nargs='?',const=True,default=False)#
-
-   
-
-    
-
     parser.add_argument('-n_cpus', default=1, type=int)
-    
     #section names encoding (pubmed&arxiv)
     parser.add_argument("-sn_embed_comb_mode", default='sum', type=str, choices=['sum', 'mean'])
-    
     ##bigbird_pegasus
     parser.add_argument("-is_encoder_decoder", type=str2bool, nargs='?',const=False,default=False)
-    
-    
-  
-
-
     args = parser.parse_args()
-    
-    
+
     #init_logger(args.log_file)
     
     if args.dataset=='arxiv' or args.dataset=='pubmed':
