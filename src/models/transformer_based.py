339c68d6dcbd1f4e9630fd12ca38cd77d304a226
Julian Moreno Schneider <jmschnei@gmail.com>
2024-01-02 11:30:22 +0100

    Initial class for BERT Classification. Work in progress.

--- src/models/transformer_based.py
+++ src/models/transformer_based.py
@@ -3,11 +3,14 @@ from mydata import *
 def classify_sections_bert(sect):
 
     # We have to get the text of the section
+    t = sect.text
 
+    # TODO Classification using trained BERT model
+    # labs = BertForTextClassification.classify(t)
+    labs = None
 
     # We have to store the labels
-
-
+    sect.labels = labs
     return sect
 
 
